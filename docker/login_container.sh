dca02cb9ea2983cc5c56e07fa2e380a0594e66fb
Steven Palma <imstevenpmwork@ieee.org>
2024-10-03 15:10:39 +0200

    chore: add check for container before login

--- docker/login_container.sh
+++ docker/login_container.sh
@@ -2,4 +2,9 @@
 
 CONTAINER_NAME=${CONTAINER_NAME:-roscon2024_workshop}
 
-docker exec -ti $CONTAINER_NAME /bin/bash
+if docker ps -q -f name="$CONTAINER_NAME" > /dev/null; then
+    docker exec -ti "$CONTAINER_NAME" /bin/bash
+else
+    echo "Error: Container $CONTAINER_NAME is not running."
+    exit 1
+fi
