007d0bc3e5e1dd0472989e04b74d561b55cea1b7
Steven Palma <imstevenpmwork@ieee.org>
2024-10-03 16:03:44 +0200

    chore(style): run spellcheck

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -6,7 +6,7 @@ IMAGE_NAME=${IMAGE_NAME:-zettascaletech/roscon2024_workshop}
 
 BASE_DIR=$(cd "$(dirname "$0")" && pwd)
 
-if [ ! "$(docker images -q ${IMAGE_NAME})" ]; then
+if [ ! "$(docker images -q "${IMAGE_NAME}")" ]; then
     echo "${IMAGE_NAME} does not exist. Creating..."
     docker build -t "$IMAGE_NAME" -f "$BASE_DIR/Dockerfile" "$BASE_DIR"
 else
--- docker/rm_image.sh
+++ docker/rm_image.sh
@@ -7,5 +7,5 @@ IMAGE_NAME=${IMAGE_NAME:-zettascaletech/roscon2024_workshop}
 if docker images | grep -q "$IMAGE_NAME"; then
     docker rmi -f "$IMAGE_NAME"
 else
-    echo "Image "$IMAGE_NAME" not found."
+    echo "Image $IMAGE_NAME not found."
 fi
