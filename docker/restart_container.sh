1c43b67e92af157bcb91f4c5670ca85b14d62ce6
Steven Palma <imstevenpmwork@ieee.org>
2024-10-14 13:09:26 +0200

    [skip ci] chore: remove unused variable

--- docker/restart_container.sh
+++ docker/restart_container.sh
@@ -3,9 +3,6 @@
 # Docker container name
 CONTAINER_NAME=${CONTAINER_NAME:-roscon2024_workshop}
 
-# Get absolute path to base directory (roscon2024_workshop/docker/ dir)
-BASE_DIR=$(cd "$(dirname "$0")" && pwd)
-
 if docker container ls -a -f name="$CONTAINER_NAME" | grep -q "$CONTAINER_NAME"; then
     # Restart the container
     docker container restart "$CONTAINER_NAME"
